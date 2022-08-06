085821984297fcb7eaced62034b54baddc504219
Ayman Elsayed <ayman.elsayed628@gmail.com>
2022-08-06 12:36:43 +0200

    fixing route naming in api.php

--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,35 @@
 <?php
 
-// use Illuminate\Http\Request;
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\api\ProductController;
-// use App\Http\Controllers\api\CommentController;
-// use App\Http\Controllers\api\RegisterController;
-// use App\Http\Controllers\Api\PostController;
-// use App\Http\Controllers\Api\UserController;
-// use App\Http\Controllers\api\ClientController;
-// use App\Http\Controllers\api\ProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\CommentController;
+use App\Http\Controllers\api\RegisterController;
+use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\api\ClientController;
+use App\Http\Controllers\api\ProfileController;
 
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-// Route::apiresource("/Products", ProductController::class);
-// Route::apiresource("/comments", CommentController::class);
-// Route::post("/Products/{id}", [ProductController::class, 'testss']);
-// Route::post("/register", [RegisterController::class, 'store']);
-// Route::post("/login", [RegisterController::class, 'login']);
-// Route::post("/logout", [RegisterController::class, 'logout']);
-// Route::apiresource('/posts', PostController::class);
-// Route::apiresource('/users', UserController::class);
 
-// Route::apiresource("/clients", ClientController::class);
 
-// Route::apiresource("/profiles", ProfileController::class);
-// Route::post("/profiles/{id}", [ProfileController::class, "updating"]);
+Route::name('api.')->group(function () {
+
+    Route::apiresource("/Products", ProductController::class);
+    Route::apiresource("/comments", CommentController::class);
+    Route::post("/Products/{id}", [ProductController::class, 'testss']);
+    Route::post("/register", [RegisterController::class, 'store']);
+    Route::post("/login", [RegisterController::class, 'login']);
+    Route::post("/logout", [RegisterController::class, 'logout']);
+    Route::apiresource('/posts', PostController::class);
+    Route::apiresource('/users', UserController::class);
+
+    Route::apiresource("/clients", ClientController::class);
+
+    Route::apiresource("/profiles", ProfileController::class);
+    Route::post("/profiles/{id}", [ProfileController::class, "updating"]);
+});
