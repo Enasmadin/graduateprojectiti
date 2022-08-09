f67b27094c3a7dc8b5e18b5dfebc8ed0dcd78780
Ayman Elsayed <ayman.elsayed628@gmail.com>
2022-08-09 16:23:40 +0200

    talabia title

--- resources/views/admin/layouts/main-header.blade.php
+++ resources/views/admin/layouts/main-header.blade.php
@@ -1,5 +1,3 @@
-
-
 <!--=================================
 header start-->
 <nav class="admin-header navbar navbar-default col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
@@ -7,23 +5,23 @@ header start-->
     <div class="text-left navbar-brand-wrapper">
         <a class="navbar-brand brand-logo"">&nbsp;
             <i class="fa fa-truck" aria-hidden="true"></i>
-            <em>طلبية</em></a>
+            <em>Talabia</em></a>
         <a class="navbar-brand brand-logo-mini"">&nbsp;
             <i class="fa fa-truck" aria-hidden="true"></i>
-            <em>طلبية</em></a>
+            <em>Talabia</em></a>
 
 
     </div>
     <!-- Top bar left -->
     <ul class="nav navbar-nav mr-auto">
         <li class="nav-item">
-            <a id="button-toggle" class="button-toggle-nav inline-block ml-50 pull-left"
-                href="javascript:void(0);"><i class="zmdi zmdi-menu ti-align-right"></i></a>
+            <a id="button-toggle" class="button-toggle-nav inline-block ml-50 pull-left" href="javascript:void(0);"><i
+                    class="zmdi zmdi-menu ti-align-right"></i></a>
         </li>
     </ul>
 
 
-        {{-- <li class="nav-item fullscreen">
+    {{-- <li class="nav-item fullscreen">
             <a id="btnFullscreen" href="#" class="nav-link"><i class="ti-fullscreen"></i></a>
         </li>
         <li class="nav-item dropdown ">
@@ -105,47 +103,44 @@ header start-->
                 </form>
             </div>
         </li> --}}
-        <li class="nav-item dropdown p-0 fixed-top d-flex flex-row">
-            <a id="navbarDropdown" class="nav-link" role="button" data-bs-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false" v-pre href="#">
-                <div class="d-flex flex-md-row-reverse">
-                    <div
-                        class="d-flex flex-column justify-content-center align-items-center text-capitalize">
-                        <span >
-                            {{ Auth::user()->name }}
-                        </span>
-
-                    </div>
-                    <div class="mx-2 d-sm-none d-md-block">
-                        <img src="{{ asset('profilepic') . '/' . auth()->user()->profile_pic }}"
-                            class="rounded-circle card-img-top"
-                            style="width:50px; height:50px; object-fit:cover">
-                    </div>
+    <li class="nav-item dropdown p-0 fixed-top d-flex flex-row">
+        <a id="navbarDropdown" class="nav-link" role="button" data-bs-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false" v-pre href="#">
+            <div class="d-flex flex-md-row-reverse">
+                <div class="d-flex flex-column justify-content-center align-items-center text-capitalize">
+                    <span>
+                        {{ Auth::user()->name }}
+                    </span>
 
                 </div>
+                <div class="mx-2 d-sm-none d-md-block">
+                    <img src="{{ asset('profilepic') . '/' . auth()->user()->profile_pic }}"
+                        class="rounded-circle card-img-top" style="width:50px; height:50px; object-fit:cover">
+                </div>
+
+            </div>
+        </a>
+
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('profiles.show', auth()->user()->id) }}">
+                {{ __('صفحتي') }}
             </a>
 
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('profiles.show', auth()->user()->id) }}">
-                    {{ __('صفحتي') }}
-                </a>
-
-                <a class="dropdown-item" href="{{ route('profiles.edit', auth()->user()->id) }}">
-                    {{ __('تعديل') }}
-                </a>
+            <a class="dropdown-item" href="{{ route('profiles.edit', auth()->user()->id) }}">
+                {{ __('تعديل') }}
+            </a>
 
 
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
+            <a class="dropdown-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
                                  document.getElementById('logout-form').submit();">
-                    {{ __('خروج') }}
-                </a>
+                {{ __('خروج') }}
+            </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                    class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </li>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+    </li>
     </ul>
 </nav>
